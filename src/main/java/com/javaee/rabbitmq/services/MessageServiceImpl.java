0176c3f809958dd2e318d4f108f9529d8b955b12
Joao Moreira <jfmoreir@thoughtworks.com>
2018-10-12 20:01:23 -0300

    Creating starting tutorial branch

--- src/main/java/com/javaee/rabbitmq/services/MessageServiceImpl.java
+++ src/main/java/com/javaee/rabbitmq/services/MessageServiceImpl.java
@@ -1,22 +1,16 @@
 package com.javaee.rabbitmq.services;
 
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
-import com.javaee.rabbitmq.config.RabbitMQConfig;
 import com.javaee.rabbitmq.domain.Message;
 
 @Service
 public class MessageServiceImpl implements MessageService{
 
-	private final RabbitTemplate rabbitTemplate;
-	 
-    public MessageServiceImpl(RabbitTemplate rabbitTemplate) {
-        this.rabbitTemplate = rabbitTemplate;
+    public MessageServiceImpl() {
     }
     
     @Override
     public void sendMessage(Message message) {
-        this.rabbitTemplate.convertAndSend(RabbitMQConfig.QUEUE_MESSAGES, message);
     }
 }
